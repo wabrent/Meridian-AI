50b92985ddce938ddb1b28a9048a7f11dd9ec355
wabrent <anatoliygranichenko@gmail.com>
2026-03-31 09:50:23 +0300

    Fix: use SHELBYNET for wallet

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -27,7 +27,7 @@ const queryClient = new QueryClient();
 
 function NetworkChecker({ children }: PropsWithChildren) {
   const [isCorrectNetwork] = useState(true);
-  const [currentNetwork] = useState("TESTNET");
+  const [currentNetwork] = useState("Shelbynet");
 
   const shelbyClient = useMemo(() => {
     const apiKey = process.env.NEXT_PUBLIC_SHELBY_API_KEY || '';
@@ -55,7 +55,7 @@ export function WalletProvider({ children }: PropsWithChildren) {
       <AptosWalletAdapterProvider
         autoConnect={false}
         dappConfig={{
-          network: Network.TESTNET,
+          network: Network.SHELBYNET,
         }}
       >
         <NetworkChecker>
